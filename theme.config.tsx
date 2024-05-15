f23989938cbdb59fdac6fcd3997c2e4d19255f91
Ryan Macalandag <rmacalandag@gmail.com>
2024-05-15 10:07:21 +1000

    first commit, bunch of pages added

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+import ftccIcon from '/public/assets/images/ftcc logo symbol blackout partial indigo.png';
+import styles from './theme-custom.module.css'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <div className={styles.logocontainer}>
+    <Image
+      src={ftccIcon}
+      alt='ftcc icon'
+      width={24}
+      height={24}
+    />  FTCC Handbook</div>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ryanmacalandag/ftcc-handbook',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: <div className={styles.footer}>Filipino Tech Community Canberra</div>,
   },
 }
 
